105e04e31701440ee699945561c188699a878525
HarrisonBath <harrison.bath@rabobank.com>
2016-07-12 16:55:21 +0100

    Add files via upload

---
+++ AllADGroups.ps1
@@ -0,0 +1,5 @@
+﻿Import-Module activedirectory
+                $user = Read-Host -Prompt "Please enter the users login"
+                Get-ADUser $user -Properties * | Select memberof | select -Expand memberof > "C:\temp\MIDTUser.txt"
+                Invoke-Item "C:\temp\MIDTUser.txt"  
+                Write-Host "Your user information is now on your screen" -ForegroundColor Green
