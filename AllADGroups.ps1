2288f44f7bd4ffd73eb40226391b476cac1d0caa
HarrisonBath <harrison.bath@rabobank.com>
2016-07-13 15:49:15 +0100

    Update AllADGroups.ps1

--- AllADGroups.ps1
+++ AllADGroups.ps1
@@ -1,5 +1,84 @@
-﻿Import-Module activedirectory
-                $user = Read-Host -Prompt "Please enter the users login"
-                Get-ADUser $user -Properties * | Select memberof | select -Expand memberof > "C:\temp\MIDTUser.txt"
-                Invoke-Item "C:\temp\MIDTUser.txt"  
-                Write-Host "Your user information is now on your screen" -ForegroundColor Green
+#LondonDealingRoomSupport
+        # File Name      : AllADGroups.ps1
+        # Author         : Harrison Bath
+
+Function Show-Menu {
+
+Param(
+[Parameter(Position=0,Mandatory=$True,HelpMessage="Enter your menu text")]
+[ValidateNotNullOrEmpty()]
+[string]$Menu,
+[Parameter(Position=1)]
+[ValidateNotNullOrEmpty()]
+[string]$Title="Menu",
+[switch]$ClearScreen
+)
+
+if ($ClearScreen) {Clear-Host}
+
+#build the menu prompt
+$menuPrompt=$title
+#add a return
+$menuprompt+="`n"
+#add an underline
+$menuprompt+="-"*$title.Length
+$menuprompt+="`n"
+#add the menu
+$menuPrompt+=$menu
+
+
+Read-Host -Prompt $menuprompt
+
+} #end function
+#Enter the menu below...
+
+$menu=@"
+
+    1. List of all users AD Groups
+
+    ============================================
+
+    Q. Quit
+
+Select 1 for the script or Q to exit
+"@
+
+#Looping and running menu until user quits
+
+Do{
+        #use a Switch construct to take action depending on what menu choice is selected.
+        
+    Switch (Show-Menu $menu "Harrison's Powershell Script" -clear) {
+    
+        "1"{
+                
+            Try{
+                    Import-Module activedirectory
+                    $user = Read-Host -Prompt "Please enter the users login"
+                    Get-ADUser $user -Properties * | Select memberof | select -Expand memberof > "C:\temp\MIDTUser.txt"
+                        Invoke-Item "C:\temp\MIDTUser.txt"  
+                            Write-Host "Your user information is now on your screen" -ForegroundColor Green
+
+                } Catch 
+                                    {
+                                        #This part will only come into place, if you type an incorrect user ID in the "try" section
+                                        Write-Host "$user does not exist, please try again." -foregroundcolor red
+                                            Sleep -seconds 3
+
+                                    }
+                      
+              
+            }
+
+        "Q"      {
+                  #Quits Script
+                  Write-Host "Goodbye" -ForegroundColor Green
+                  Return
+                 }
+
+                        Default {Write-Warning "Invalid Choice. Try again."
+                        sleep -seconds 2
+                                }
+
+                                                                    } #switch
+   } While ($True)
