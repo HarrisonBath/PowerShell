b09466595977ee13edae161d8b95d2423d58874f
HarrisonBath <harrison.bath@rabobank.com>
2016-07-13 09:19:54 +0100

    Add files via upload

---
+++ Get-ADGroups.ps1
@@ -0,0 +1,71 @@
+﻿#LondonDealingRoomSupport
+        # File Name      : Get-ADGroups.ps1
+        # Author         : Harrison Bath
+
+Function Show-Menu {
+
+Param(
+[Parameter(Position=0,Mandatory=$True,HelpMessage="Enter your menu text")]
+[ValidateNotNullOrEmpty()]
+[string]$Menu,
+[Parameter(Position=1)]
+[ValidateNotNullOrEmpty()]
+[string]$Title="Menu",
+[switch]$ClearScreen
+)
+
+if ($ClearScreen) {Clear-Host}
+
+#build the menu prompt
+$menuPrompt=$title
+#add a return
+$menuprompt+="`n"
+#add an underline
+$menuprompt+="-"*$title.Length
+$menuprompt+="`n"
+#add the menu
+$menuPrompt+=$menu
+
+
+Read-Host -Prompt $menuprompt
+
+} #end function
+#Enter the menu below...
+
+$menu=@"
+
+    1. User's AD Groups
+
+    ============================================
+
+    Q. Quit
+
+Select 1 for the script or Q to exit
+"@
+
+#Looping and running menu until user quits
+
+Do{
+        #use a Switch construct to take action depending on what menu choice is selected.
+        
+    Switch (Show-Menu $menu "Harrison's Powershell Script" -clear) {
+    
+        "1"{Import-Module activedirectory
+                $user = Read-Host -Prompt "Please enter the users login"
+                Get-ADUser $user -Properties * | Select memberof | select -Expand memberof > "C:\temp\MIDTUser.txt"
+                Invoke-Item "C:\temp\MIDTUser.txt"  
+                Write-Host "Your user information is now on your screen" -ForegroundColor Green
+                 }
+
+        "Q" {
+                #Quits Script
+                Write-Host "Goodbye" -ForegroundColor Green
+                Return
+            }
+
+                        Default {Write-Warning "Invalid Choice. Try again."
+                        sleep -seconds 2
+                                }
+
+         } #switch
+         } While ($True)
