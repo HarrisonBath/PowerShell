542261259d42a3d5f7f645ddd97d74f796050533
HarrisonBath <harrison.bath@rabobank.com>
2016-07-13 09:21:29 +0100

    Add files via upload

---
+++ LoggedOnUser.ps1
@@ -0,0 +1,13 @@
+﻿Import-Module activedirectory
+                $PcName = Read-Host -prompt "Please enter the hostname of the machine"
+                $colComputers = Get-ADComputer $PcName -Properties description | ForEach-Object {$_.Name} 
+                foreach ($strComputer in $colComputers)
+                { 
+                $description = Get-ADComputer $strComputer -Properties description
+                $Username = (Get-WmiObject -ComputerName $strComputer -class Win32_computersystem).username}
+                if($username -like "*Rabo*") 
+                {Write-Host $strComputer $username " | Description: " $description.description -ForegroundColor Green}
+                Else
+                {$description = Get-ADComputer $strComputer -Properties description
+                Write-Host "Nobody logged in on $StrComputer | Description: " $description.description -ForegroundColor Red} 
+             sleep -seconds 5
