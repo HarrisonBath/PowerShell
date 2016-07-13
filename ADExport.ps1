46eedace05e2855af49d7870be9f5950e8fffd15
HarrisonBath <harrison.bath@rabobank.com>
2016-07-13 15:40:15 +0100

    Update ADExport.ps1

--- ADExport.ps1
+++ ADExport.ps1
@@ -1,4 +1,4 @@
-﻿        #File name : ADExport
+        #File name : ADExport
         #Author : Harrison Bath
 
 #This script exports AD Information for all LO* Machines
@@ -7,7 +7,7 @@
             #This part creates csv file with all LO* Machines in AD
             $Filename = Read-Host -prompt "Enter today's date"
             Get-ADComputer -Filter {(enabled -eq "true") -and (name -like "LO*") } -Properties Description | Select-Object Name, Description | Export-CSV \\LOND121718\C$\TEMP\AD\$Filename.csv -NoTypeInformation -Encoding UTF8
-   
+                                                                                                                                                  #this part exports the csv file into my TEMP
                         Write-Host "AD Information has been exported" -ForegroundColor Green
                         Sleep -Seconds 3
                         
